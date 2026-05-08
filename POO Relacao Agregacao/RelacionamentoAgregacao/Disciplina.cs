08ad1184900ffcae0bb24ca6cdb34633a541be7c
André Moreira <afrmscb@gmail.com>
2026-05-08 11:29:26 +0100

    Add files via upload

---
+++ POO Relacao Agregacao/RelacionamentoAgregacao/Disciplina.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelacionamentoAgregacao
+{
+    internal class Disciplina
+    {
+        public string  Nome { get; set; }
+
+        public List<Estudante> Estudantes { get; set; }
+
+        public Disciplina()
+        {
+            Estudantes = new List<Estudante>();
+        }
+
+        //Método para adicionar estudante á disciplina
+
+        public void AdicionarEstudante(Estudante e)
+        {
+            Estudantes.Add(e);
+        }
+    }
+}
