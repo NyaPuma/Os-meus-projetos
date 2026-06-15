96ef68d24697a89a97196582e895f4c80ebb877c
André Moreira <afrmscb@gmail.com>
2026-06-15 16:26:43 +0100

    feat: initialize FirstApp project and implement basic greeting functionality
    - Set up the project structure using Gradle Kotlin DSL and Version Catalog (`libs.versions.toml`).
    - Enabled View Binding in the app module configuration.
    - Implemented `MainActivity` logic to capture user input and display a personalized greeting.
    - Created `activity_main` layout using `ConstraintLayout` with an `EditText`, `Button`, and `TextView`.
    - Configured project-wide settings including toolchains, configuration cache, and Material3 themes.
    - Added standard application assets, including launcher icons and vector drawables.

---
+++ Android/FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() { // declaração da classe
+    private lateinit var binding: ActivityMainBinding // inicialização da classe
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root) // diz ao android qual o layout XML a utilizar neste ecrâ
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//
+//        // ligação às views
+//        val texto_nome = findViewById<EditText>(R.id.editText_Nome)
+//        val botao = findViewById<Button>(R.id.btn_ola)
+//        val res = findViewById<TextView>(R.id.textView_resultado)
+//
+//        // Evento do clique do botão
+//        botao.setOnClickListener {
+//            val nome = texto_nome.text.toString()
+//            res.text = "Olá $nome"
+//        }
+
+        // com view binding
+        binding.btnOla.setOnClickListener {
+            val nome = binding.editTextNome.text.toString()
+            binding.textViewResultado.text = "Olá $nome"
+        }
+    }
+}
